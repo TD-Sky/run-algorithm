f4206e1452e89ffbfe0b8541051bb599ef63897a
Randal <1752963246@qq.com>
2021-12-21 19:03:35 +0800

    哈希表

---
+++ hash_map/src/lib.rs
@@ -0,0 +1,38 @@
+#![feature(drain_filter)]
+
+use std::hash::Hash;
+use std::mem;
+
+mod chain_hash_map;
+mod linear_hash_map;
+
+pub use self::chain_hash_map::ChainHashMap;
+pub use self::linear_hash_map::LinearHashMap;
+
+#[allow(dead_code)]
+struct Node<K: Eq + Hash, V> {
+    key: K,
+    value: V,
+    hash_code: u64,
+}
+
+impl<K, V> Node<K, V>
+where
+    K: Eq + Hash,
+{
+    fn new(key: K, value: V, hash_code: u64) -> Self {
+        Self {
+            key,
+            value,
+            hash_code,
+        }
+    }
+
+    fn replace(&mut self, val: V) -> V {
+        mem::replace(&mut self.value, val)
+    }
+
+    fn into_value(self) -> V {
+        self.value
+    }
+}
