7a0b96f120448d8cbfadcb88c11bb395283bfe34
Collide <44722470+TD-Sky@users.noreply.github.com>
2022-04-07 01:18:31 +0800

    优化全排列

--- combinatorics/src/lib.rs
+++ combinatorics/src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(is_sorted)]
-
 mod permutation;
 
 pub use self::permutation::next_permutation;
--- combinatorics/src/permutation.rs
+++ combinatorics/src/permutation.rs
@@ -1,5 +1,3 @@
-use std::cmp::PartialOrd;
-
 // 全排列生成器，按字典序进行
 // 前提：切片的元素互异
 // 结果：
@@ -7,35 +5,33 @@ use std::cmp::PartialOrd;
 // - None —— 切片长度 ≤ 1 或 降序排列
 pub fn next_permutation<T>(seq: &mut [T]) -> Option<&[T]>
 where
-    T: Ord,
+    T: PartialOrd,
 {
-    match seq.is_sorted_by(|lhs, rhs| PartialOrd::partial_cmp(rhs, lhs)) {
-        true => None,
-        false => {
-            // 寻找最右侧、符合小于关系的下标
-            let rmost_lt = (0..seq.len() - 1)
-                .rposition(|i| seq[i] < seq[i + 1])
-                .unwrap();
-
-            // 寻找 rmost_lt 的最小上确界之下标
-            // 由于 rmost_lt 的性质，supermum 至少比它多1
-            let supermum = (rmost_lt + 1..seq.len())
-                .rfind(|&i| seq[i] > seq[rmost_lt])
-                .unwrap();
-
-            // 连同下文
-            // 严格控制排列序数+1
-            seq.swap(rmost_lt, supermum);
-
-            // rmost_lt 之后的部分按升序列举
-            let mut rest = (rmost_lt + 1..seq.len()).into_iter();
-            while let [Some(head), Some(tail)] = [rest.next(), rest.next_back()] {
-                seq.swap(head, tail);
-            }
-
-            Some(seq)
-        }
+    if seq.len() < 2 {
+        return None;
     }
+
+    // 寻找最右侧、符合小于关系的下标
+    // 找不到就说明传入数列字典序最大了
+    let rmost_lt = (0..seq.len() - 1).rfind(|&i| seq[i] < seq[i + 1])?;
+
+    // 寻找 rmost_lt 的最小上确界之下标
+    // 由于 rmost_lt 的性质，supermum 至少比它多1
+    let supermum = (rmost_lt + 1..seq.len())
+        .rfind(|&i| seq[i] > seq[rmost_lt])
+        .unwrap();
+
+    // 连同下文
+    // 严格控制排列序数+1
+    seq.swap(rmost_lt, supermum);
+
+    // rmost_lt 之后的部分按升序列举
+    let mut rest = (rmost_lt + 1..seq.len()).into_iter();
+    while let [Some(head), Some(tail)] = [rest.next(), rest.next_back()] {
+        seq.swap(head, tail);
+    }
+
+    return Some(seq);
 }
 
 #[cfg(test)]
